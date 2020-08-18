4064d3059d6e0cdafd3b9322ccfd582318bd01ac
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-08-18 15:40:14 +0200

    fix undefined behavior in PNG decoder

--- torchvision/csrc/cpu/image/readpng_cpu.cpp
+++ torchvision/csrc/cpu/image/readpng_cpu.cpp
@@ -12,44 +12,45 @@ torch::Tensor decodePNG(const torch::Tensor& data) {
 #else
 #include <png.h>
 
-torch::Tensor decodePNG(const torch::Tensor& data) {
-  auto png_ptr =
-      png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-  TORCH_CHECK(png_ptr, "libpng read structure allocation failed!")
-  auto info_ptr = png_create_info_struct(png_ptr);
-  if (!info_ptr) {
-    png_destroy_read_struct(&png_ptr, nullptr, nullptr);
-    // Seems redundant with the if statement. done here to avoid leaking memory.
-    TORCH_CHECK(info_ptr, "libpng info structure allocation failed!")
-  }
 
-  auto datap = data.accessor<unsigned char, 1>().data();
+namespace {
+  struct Reader {
+      png_const_bytep ptr;
+  };
+
+  static void readCallback(png_structp png_ptr, png_bytep output, png_size_t bytes)
+  {
+    Reader *reader = static_cast<Reader*>(png_get_io_ptr(png_ptr));
+    std::copy(reader->ptr, reader->ptr + bytes, output);
+    reader->ptr += bytes;
+  }
+}
+
+// See the comments in the jpeg decoder for why we need to do this. Spoiler:
+// undefined behavior with setjmp and longjmp.
+static void actualDecodePNG(const torch::Tensor& data,
+        png_structp *png_ptr,
+        png_infop *info_ptr,
+        torch::Tensor *output,
+        Reader *Reader
+        )
+{
+  unsigned char *datap = data.accessor<unsigned char, 1>().data();
 
   if (setjmp(png_jmpbuf(png_ptr)) != 0) {
-    png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
     TORCH_CHECK(false, "Internal error.");
   }
-  auto is_png = !png_sig_cmp(datap, 0, 8);
+  bool is_png = !png_sig_cmp(datap, 0, 8);
   TORCH_CHECK(is_png, "Content is not png!")
+  reader->ptr = png_const_bytep(datap) + 8;
 
-  struct Reader {
-    png_const_bytep ptr;
-  } reader;
-  reader.ptr = png_const_bytep(datap) + 8;
-
-  auto read_callback =
-      [](png_structp png_ptr, png_bytep output, png_size_t bytes) {
-        auto reader = static_cast<Reader*>(png_get_io_ptr(png_ptr));
-        std::copy(reader->ptr, reader->ptr + bytes, output);
-        reader->ptr += bytes;
-      };
   png_set_sig_bytes(png_ptr, 8);
-  png_set_read_fn(png_ptr, &reader, read_callback);
+  png_set_read_fn(png_ptr, reader, readCallback);
   png_read_info(png_ptr, info_ptr);
 
   png_uint_32 width, height;
   int bit_depth, color_type;
-  auto retval = png_get_IHDR(
+  png_uint_32 retval = png_get_IHDR(
       png_ptr,
       info_ptr,
       &width,
@@ -61,24 +62,40 @@ torch::Tensor decodePNG(const torch::Tensor& data) {
       nullptr);
 
   if (retval != 1) {
-    png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
     TORCH_CHECK(retval == 1, "Could read image metadata from content.")
   }
   if (color_type != PNG_COLOR_TYPE_RGB) {
-    png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
     TORCH_CHECK(
         color_type == PNG_COLOR_TYPE_RGB, "Non RGB images are not supported.")
   }
 
-  auto tensor =
-      torch::empty({int64_t(height), int64_t(width), int64_t(3)}, torch::kU8);
-  auto ptr = tensor.accessor<uint8_t, 3>().data();
-  auto bytes = png_get_rowbytes(png_ptr, info_ptr);
+  uint8_t *ptr = tensor.accessor<uint8_t, 3>().data();
+  size_t bytes = png_get_rowbytes(png_ptr, info_ptr);
   for (decltype(height) i = 0; i < height; ++i) {
     png_read_row(png_ptr, ptr, nullptr);
     ptr += bytes;
   }
+}
+
+
+torch::Tensor decodePNG(const torch::Tensor& data) {
+  auto png_ptr =
+      png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  TORCH_CHECK(png_ptr, "libpng read structure allocation failed!")
+  auto info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr) {
+    // Seems redundant with the if statement. done here to avoid leaking memory.
+    TORCH_CHECK(info_ptr, "libpng info structure allocation failed!")
+  }
+
+  auto tensor =
+      torch::empty({int64_t(height), int64_t(width), int64_t(3)}, torch::kU8);
+
+  Reader reader;
+  actualDecodePNG(data, png_ptr, info_ptr, &tensor, &reader);
+
   png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
+
   return tensor;
 }
 #endif // PNG_FOUND
